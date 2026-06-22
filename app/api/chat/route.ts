5b72bb5d21fbbebd98a750d28af446cc3c52714b
Min Myat Thu <minmyatthu3644@gmail.com>
2026-06-22 11:06:44 +0200

    change ai model

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -51,7 +51,7 @@ export async function POST(req: NextRequest) {
 
   try {
     const response = await ai.models.generateContent({
-      model: 'gemini-2.0-flash',
+      model: 'gemini-2.5-flash-lite',
       contents,
       config: {
         tools: [{ googleSearch: {} }],
